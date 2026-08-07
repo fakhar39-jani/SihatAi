adf1984379f64737c5c330ae287b3a6b063a322c
Humaid Muhammad <humaidmuhammad039@gmail.com>
2026-08-07 17:39:51 +0500

    Fix trust proxy for Render

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ import apiRouter from "./routes/api.js";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || "*" }));
